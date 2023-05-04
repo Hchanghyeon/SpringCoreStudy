435a34160b55467bc5a434fcf886de3ff5bee340
changhyeon <changhyeon.h@kakao.com>
2023-05-04 21:57:51 +0900

    Feat: ComponentScan Add

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration // 설정 정보
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+
+}
