fae81ff4a84976454a2953891d077139f3d66202
changhyeon <changhyeon.h@kakao.com>
2023-05-05 17:08:35 +0900

    Fix: Two Bean Select, Qualifier or Primary

--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -1,10 +1,12 @@
 package hello.core.order;
 
+import hello.core.CoreApplication;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,6 +17,12 @@ class OrderServiceImplTest {
     void createOrder() {
         OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
         MemberServiceImpl memberService = new MemberServiceImpl(new MemoryMemberRepository());
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(CoreApplication.class);
+        String[] beanNames = ac.getBeanDefinitionNames();
+        for(String beanName : beanNames){
+            System.out.println("beanName = " + beanName);
+        }
 
         Member member = new Member(1L, "창현", Grade.VIP);
         memberService.join(member);
